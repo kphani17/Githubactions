5f36613448ecaed1d322c0606dce4ffaf121c1d0
kphani17 <kphani7@outlook.com>
2025-10-15 22:10:44 +0530

    python code updated

--- abcd.py
+++ abcd.py
@@ -1,1 +1,4 @@
+import json
 print('Hello! all, This is python script')
+data = json.load(open('config.json'))
+print(f'ENVIRONMENT={data[\"environment\"]}\\nAPP_NAME={data[\"app_name\"]}\\nREGION={data[\"region\"]}')
